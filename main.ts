11afabbb299417d42451b8877f6ca7c6b2e5007a
Mariasharifuddin <161883333+Mariasharifuddin@users.noreply.github.com>
2024-04-22 22:32:48 +0500

    push my todo list to github

---
+++ main.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todolist = [];
+let conditions = true;
+
+console.log(chalk.yellow.bold
+("\n \t welcome to codewithmariya - Todo-list Application"));
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: "Enter your New Task :"
+        }
+    ]);
+todolist.push(addTask.task);
+console.log(`${addTask.task} Task added in Todo-list sucessfully`);
+
+let addMoreTask = await inquirer.prompt([
+    {
+        name: "addmore",
+        type: "confirm",
+        message: "Do you want to add more task ?",
+        default: "False",
+    }
+]);
+conditions = addMoreTask.addmore
+}
+console.log("your updated Todo-list:", todolist);
